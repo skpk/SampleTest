16b68da3ce0f17e1fe40e3dba3274ce321129493
jagadish12 <jagadesh.manchala@gmail.com>
2019-04-25 14:44:54 +0530

    Update Prime.java

--- SampleTest/Prime.java
+++ SampleTest/Prime.java
@@ -19,5 +19,6 @@ public class Prime {
             System.out.println("this is jagadish and Comitted a New Change"); 
         else
             System.out.println(num + " is not a prime number.");
+            System.out.println("this is jagadish in the if else loop");
     }
 }
